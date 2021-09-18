78b32c7d4756f3e9102c1722d1462e92789dac43
NukaGrizz <nukagrizz@gmail.com>
2021-09-18 15:23:02 -0400

    change js for signup page from login page

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,27 +1,3 @@
-async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
   async function signupFormHandler(event) {
     event.preventDefault();
   
@@ -48,5 +24,5 @@ async function loginFormHandler(event) {
     }
   }
   
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
   
