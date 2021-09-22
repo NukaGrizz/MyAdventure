40d19da9f90bf03c5b3c758528d75933c3c4838e
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-09-22 16:29:00 -0400

    added routes to get all posts from one user

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -6,7 +6,7 @@ async function loginFormHandler(event) {
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
-        method: 'get',
+        method: 'post',
         body: JSON.stringify({
           email,
           password
