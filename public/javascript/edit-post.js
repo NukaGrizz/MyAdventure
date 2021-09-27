9a5d3cfee50e27369973302ec040740be2120cb9
NukaGrizz <nukagrizz@gmail.com>
2021-09-26 22:58:48 -0400

    add Restful API comments to api routes

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,9 +1,19 @@
 async function editFormHandler(event) {
     event.preventDefault();
-  
+
     const title = document.querySelector('textarea[name="post-title"]').value.trim();
     const post_text = document.querySelector('textarea[name="post-url"]').value.trim();
+    if( title !== "" || title !== null ) {
+      title = document.querySelector('textarea[name="post-title"]').placeholder;
+    } else if( post_text !== "" || post_text !== null) {
+      post_text = document.querySelector('textarea[name="post-url"]').placeholder;
+    } else {
+      
+    }
+    console.log("--------------------------------------------------------");
     console.log(post_text);
+    console.log(title);
+    console.log("--------------------------------------------------------");
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
